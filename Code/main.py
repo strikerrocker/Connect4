720817470ff30278f954d45b6b344c545059e347
StrikerRocker <strikerrockerplays@gmail.com>
2022-11-16 18:34:58 +0530

    Fix bugs and add menu

--- Code/main.py
+++ Code/main.py
@@ -1,10 +1,12 @@
+import math
+import socket
+import sys
+from tkinter import *
 from tkinter import simpledialog
+import pygame_menu
 import numpy as np
 import pygame
-import sys
-import math
-from tkinter import *
-import socket
+from pygame_menu import themes
 
 BLUE = (0, 0, 255)
 BACKROUND_COLOR = (135, 206, 235)
@@ -14,16 +16,9 @@ YELLOW = (255, 255, 0)
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
-game_over = False
-turn = 0
+
 player = 0
-# initalize pygame
-
-
-# define our screen size
 SQUARESIZE = 100
-
-# define width and height of board
 width = COLUMN_COUNT * SQUARESIZE
 height = (ROW_COUNT + 1) * SQUARESIZE
 
@@ -31,10 +26,12 @@ size = (width, height)
 
 RADIUS = int(SQUARESIZE / 2 - 5)
 
-root = Tk()
-s = socket.socket()
+s = None
 conn = None
 
+pygame.init()
+surface = pygame.display.set_mode(size)
+font = pygame.font.SysFont("monospace", 50)
 
 def create_board():
     board = np.zeros((ROW_COUNT, COLUMN_COUNT))
@@ -43,6 +40,7 @@ def create_board():
 
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
+    draw_board(board)
 
 
 def is_valid_location(board, col):
@@ -53,10 +51,6 @@ def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
             return r
-
-
-def print_board(board):
-    print(np.flip(board, 0))
 
 
 def winning_move(board, piece):
@@ -109,12 +103,12 @@ def draw_board(board):
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             pygame.draw.rect(
-                screen,
+                surface,
                 BLUE,
                 (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE),
             )
             pygame.draw.circle(
-                screen,
+                surface,
                 BACKROUND_COLOR,
                 (
                     int(c * SQUARESIZE + SQUARESIZE / 2),
@@ -127,7 +121,7 @@ def draw_board(board):
         for r in range(ROW_COUNT):
             if board[r][c] == 1:
                 pygame.draw.circle(
-                    screen,
+                    surface,
                     RED,
                     (
                         int(c * SQUARESIZE + SQUARESIZE / 2),
@@ -137,7 +131,7 @@ def draw_board(board):
                 )
             elif board[r][c] == 2:
                 pygame.draw.circle(
-                    screen,
+                    surface,
                     YELLOW,
                     (
                         int(c * SQUARESIZE + SQUARESIZE / 2),
@@ -148,51 +142,62 @@ def draw_board(board):
     pygame.display.update()
 
 
+def render_txt(txt, color=(0, 0, 0)):
+    pygame.draw.rect(surface, BACKROUND_COLOR, (0, 0, width, SQUARESIZE))
+    label = font.render(txt, 1, color)
+    surface.blit(label, (20, 10))
+    pygame.display.update()
+
+
 def start_game():
-    pygame.init()
-    global screen, board, game_over, turn, player, conn
-    myfont = pygame.font.SysFont("monospace", 75)
-    screen = pygame.display.set_mode(size)
+    global surface, board, player, conn
     board = create_board()
-    # Calling function draw_board again
     draw_board(board)
     pygame.display.update()
     title = "Server Game" if player == 1 else "Client Game"
-    pygame.display.set_caption(title)
     print(title)
+    skip = False
+    game_over=False
+    turn = 0
     while not game_over:
+        # Wait for other player input
         if turn != (player - 1):
-            row = int(conn.recv(1024).decode())
+            render_txt("Other user turn")
             col = int(conn.recv(1024).decode())
-            print("Recieved input from other user", row, col)
+            print("Recieved input from other user : ", col)
+            row = get_next_open_row(board, col)
             drop_piece(board, row, col, turn + 1)
+            render_txt("")
             if winning_move(board, turn + 1):
-                label = myfont.render("You lost :(", 1, RED)
-                screen.blit(label, (40, 10))
+                render_txt("You lost :(", RED)
                 game_over = True
-            draw_board(board)
             turn += 1
             turn = turn % 2
+            skip = True
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
             if event.type == pygame.MOUSEMOTION:
-                pygame.draw.rect(screen, BACKROUND_COLOR, (0, 0, width, SQUARESIZE))
+                pygame.draw.rect(surface, BACKROUND_COLOR, (0, 0, width, SQUARESIZE))
                 posx = event.pos[0]
-                if turn == 0:
-                    pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+                if player == 1:
+                    pygame.draw.circle(
+                        surface, RED, (posx, int(SQUARESIZE / 2)), RADIUS
+                    )
                 else:
                     pygame.draw.circle(
-                        screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS
+                        surface, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS
                     )
             pygame.display.update()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pygame.draw.rect(screen, BACKROUND_COLOR, (0, 0, width, SQUARESIZE))
-                # Ask for Player 1 Input
-
+                # Skip if other user has played
+                if skip:
+                    continue
+                pygame.draw.rect(surface, BACKROUND_COLOR, (0, 0, width, SQUARESIZE))
+                # Local Player user input
                 if turn == (player - 1):
                     posx = event.pos[0]
                     col = int(math.floor(posx / SQUARESIZE))
@@ -200,60 +205,92 @@ def start_game():
                     if is_valid_location(board, col):
                         row = get_next_open_row(board, col)
                         drop_piece(board, row, col, player)
-                        conn.send(str(row).encode())
+                        print("Sending ", col)
                         conn.send(str(col).encode())
                         if winning_move(board, player):
-                            label = myfont.render("You win!!", 1, RED)
-                            screen.blit(label, (40, 10))
+                            render_txt("You win!!", RED)
                             game_over = True
-
-                # print_board(board)
-                draw_board(board)
-
-                turn += 1
-                turn = turn % 2
-
+                        turn += 1
+                        turn = turn % 2
+        skip = False
         if game_over:
-            pygame.time.wait(3000)
+            pygame.time.wait(5000)
 
 
 def server():
-    root.withdraw()
-    global player, conn
-    # ip = simpledialog.askstring("IP Address","Enter IP Address to connect to")
-    # port = simpledialog.askinteger("Port No","Enter port to listen from")
-    ip = "127.0.0.1"
-    port = 1234
-    s.bind((ip, port))
-    s.listen(5)
-    c, addr = s.accept()
-    player = 1
-    conn = c
-    start_game()
-    c.close()
-    s.close()
+    ip = socket.gethostbyname(socket.gethostname())
+    def play():
+        s=socket.socket()
+        port = int(serverGUI.get_widget("port").get_value())
+        global player, conn
+        serverGUI.update
+        print("Listening in ip {} and in port {}".format(ip, port))
+        s.bind((ip, port))
+        try:
+            s.listen(5)
+            c, addr = s.accept()
+            print("Connected to client with address ", addr)
+            player = 1
+            conn = c
+            start_game()
+            c.close()
+            main_menu.reset(1)
+        except Exception as e:
+            print(e)
+            serverGUI.get_widget("label").set_title("Connection Error")
+        s.close()
+
+    serverGUI = pygame_menu.Menu(
+        "Server Menu", size[0], size[1], theme=themes.THEME_DARK
+    )
+    serverGUI.add.label("",label_id="label")
+    serverGUI.add.label("Server IP : "+ip)
+
+    serverGUI.add.text_input(
+        "Port to host :", default=9999, type="input-int", maxchar=5, textinput_id="port"
+    )
+    serverGUI.add.button("Play", play)
+    main_menu._open(serverGUI)
 
 
 def client():
-    root.withdraw()
-    global player, conn
-    # ip = simpledialog.askstring("IP Address","Enter IP Address to connect to")
-    # port = simpledialog.askinteger("Port No","Enter port to connect to")
-    ip = "127.0.0.1"
-    port = 1234
-    s.connect((ip, port))
-    player = 2
-    conn = s
-    start_game()
-    s.close()
+    def play():
+        s=socket.socket()
+        ip = clientGUI.get_widget("ip").get_value()
+        port = int(clientGUI.get_widget("port").get_value())
+        global player, conn
+        print("Connecting to ip {} and in port {}".format(ip, port))
+        try:
+            s.connect((ip, port))
+            player = 2
+            conn = s
+            start_game()
+            main_menu.reset(1)
+        except Exception as e:
+            print(e)
+            clientGUI.get_widget("label").set_title("Can't connect")
+        s.close()
+        
+
+    clientGUI = pygame_menu.Menu(
+        "Client Menu", size[0], size[1], theme=themes.THEME_DARK
+    )
+    clientGUI.add.label("",label_id="label")
+    clientGUI.add.text_input("IP to connect :", default="192.168.56.1", textinput_id="ip")
+    clientGUI.add.text_input(
+        "Port to connect :",
+        default=9999,
+        type="input-int",
+        maxchar=5,
+        textinput_id="port",
+    )
+    clientGUI.add.button("Play", play)
+    main_menu._open(clientGUI)
 
 
-# Get user input
-txt = Text(root, cnf={"height": 1})
-txt.insert(INSERT, "Is this server or client?")
-txt.pack()
-t = Button(root, text="Server", command=server)
-t.pack()
-q = Button(root, text="Client", command=client)
-q.pack()
-root.mainloop()
+pygame.display.set_caption("Connect 4 Game")
+main_menu = pygame_menu.Menu("Main Menu", size[0], size[1], theme=themes.THEME_DARK)
+main_menu.add.button("Server", server)
+main_menu.add.button("Client", client)
+main_menu.add.button("Quit", pygame_menu.events.EXIT)
+main_menu.mainloop(surface)
